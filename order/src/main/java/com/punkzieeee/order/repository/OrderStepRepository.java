b5006f2d86d56f22db682717307de834aa381bca
punkzieeee <fajaragung4to11@gmail.com>
2024-01-19 11:07:28 +0700

    refactor: cleanup unused file

--- order/src/main/java/com/punkzieeee/order/repository/OrderStepRepository.java
+++
@@ -1,9 +0,0 @@
-package com.punkzieeee.order.repository;
-
-import org.springframework.data.r2dbc.repository.R2dbcRepository;
-
-import com.punkzieeee.order.model.OrderStep;
-
-public interface OrderStepRepository extends R2dbcRepository<OrderStep, Long>{
-    
-}
